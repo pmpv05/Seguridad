187b5ef755b780cbabbc46da8e8c2453a5499e62
Pablo Piñeiro <pmpv05@hotmail.com>
2019-06-27 20:57:24 -0300

    Mejora

--- web/src/app/components/login/login.component.ts
+++ web/src/app/components/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, ChangeDetectorRef } from '@angular/core';
+import { Component } from '@angular/core';
 import { ApiService } from '../../services/api.service';
 import { Router } from '@angular/router';
 import { UsuarioModel } from 'src/app/models/usuario.model';
--- web/src/app/components/navbar/navbar.component.ts
+++ web/src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, SimpleChange, OnChanges, SimpleChanges } from '@angular/core';
+import { Component } from '@angular/core';
 import { ApiService } from 'src/app/services/api.service';
 import { Router } from '@angular/router';
 
