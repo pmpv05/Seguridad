efbfbb4aac21781af2332cc425a2e7ce666681aa
Pablo Piñeiro <pmpv05@hotmail.com>
2019-06-27 20:51:42 -0300

    Cambio de nombre file a cifrado

---
+++ web/src/app/components/files/cifrado.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cifrado',
+  templateUrl: './cifrado.component.html'
+})
+
+export class CifradoComponent implements OnInit {
+
+  clave: string;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  cifrar() {
+  }
+
+  estaCifrado() {
+  }
+
+  descifrar() {
+  }
+
+  firmar() {
+  }
+}
