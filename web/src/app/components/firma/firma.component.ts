7383b1c2206f13c8459fc9b92784fc047b629020
Pablo Piñeiro <pmpv05@hotmail.com>
2019-06-27 20:51:14 -0300

    Agregar pantallas para firmar y verificar firmas

---
+++ web/src/app/components/firma/firma.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-firma',
+  templateUrl: './firma.component.html'
+})
+export class FirmaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ web/src/app/components/verificarfirma/verificarfirma.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-verificarfirma',
+  templateUrl: './verificarfirma.component.html'
+})
+export class VerificarfirmaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
